745eb3b270202083b5c6e25569a90392858685f2
decanus <dean.eigenmann@icloud.com>
2019-03-31 03:28:27 +0200

    more

--- Sources/Multihash/Type.swift
+++ Sources/Multihash/Type.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Type: UInt8 {
-    case id = 0x0
+    case id = 0x00
     case sha1 = 0x11
     case sha2256 = 0x12
     case sha2512 = 0x13
@@ -9,6 +9,12 @@ enum Type: UInt8 {
     case sha3384 = 0x15
     case sha3256 = 0x16
     case sha3224 = 0x17
+    case keccak224 = 0x1A
+    case keccak256 = 0x1B
+    case keccak384 = 0x1C
+    case keccak512 = 0x1D
+    case shake128 = 0x18
+    case shake256 = 0x19
 
     // @todo
 }
